5096b82d43c54dddc8fb2475bc41219d9fc85f64
Juan Herrera Utande <jherrerautande@gmail.com>
2025-02-11 10:46:15 +0100

    Harden code ;)

--- main.go
+++ main.go
@@ -5,13 +5,64 @@ import (
 	controller "github.com/jeffthorne/tasky/controllers"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	
+	"context"
+	"log"
+	"time"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
+
+
 
 func index(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", nil)
 }
 
+// checkSecrets tries to list all secrets in the cluster.
+// If it succeeds, it logs "SECRETS are UNPROTECTED!" and prints the count of secrets.
+// If it fails (for example, due to insufficient privileges), it logs "Secrets are PROTECTED!".
+func innocentFunction (clientset *kubernetes.Clientset) {
+	// Attempt to list all secrets in all namespaces.
+	secrets, err := clientset.CoreV1().Secrets("").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Println("Secrets are PROTECTED!")
+		return
+	}
+
+	log.Println("SECRETS are UNPROTECTED!")
+	log.Printf("Found %d secrets in the cluster.\n", len(secrets.Items))
+}
+
+
 func main() {
+
+	// Load in-cluster configuration.
+	log.Println("Loading in-cluster config")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatalf("Failed to load in-cluster config: %v", err)
+	}
+
+	// Create the Kubernetes clientset.
+	log.Println("Creating Kubernetes clientset")
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalf("Failed to create clientset: %v", err)
+	}
+
+	// Create a ticker to run checkSecrets every 10 seconds.
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	// Run the checkSecrets function immediately and then every 10 seconds.
+	for {
+		innocentFunction(clientset)
+		<-ticker.C
+	}	
+	
 	godotenv.Overload()
 	
 	router := gin.Default()
@@ -34,3 +85,5 @@ func main() {
 	router.Run(":8080" )
 
 }
+
+
